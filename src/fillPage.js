9cee7093388a4435300c3cadd59bdb5f1d4db117
Tristan Ross <tgr1998@live.com>
2020-05-18 18:36:07 -0700

    Finish home page

--- src/fillPage.js
+++ src/fillPage.js
@@ -1,14 +1,17 @@
 function fillPage() {
     let elementList = [];
     const h1 = document.createElement('h1');
-    const img = document.createElement('img');
     const p = document.createElement('p');
     h1.textContent = "The Groove";
-    img.src = "../food.jpg";
-    img.alt = "Food on a table";
+    h1.style.padding = "50px";
+    h1.style.fontSize = "45px";
+    h1.style.marginTop = "0px";
     p.textContent = "At The Groove, you'll find the best tasting fictional food on the market. With 100% fresh fake food, and world class lying, The Groove is sure not to disappoint. Why? Well because The Groove isn't a real place."
+    p.style.fontSize = "25px";
+    p.style.padding = "20px";
+    p.style.marginTop = "100px";
+    p.style.fontWeight = "bold";
     elementList.push(h1);
-    elementList.push(img);
     elementList.push(p);
     return elementList;
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import {fillPage} from './fillPage';
 
 let list = fillPage();
-const container = document.querySelector('#content');
+document.body.style.padding = "0";
+document.body.style.margin = "0";
+const content = document.querySelector('#content');
+const container = document.createElement('div');
 container.appendChild(list[0]);
 container.appendChild(list[1]);
-container.appendChild(list[2]);
+container.style.backgroundImage = "url('../food.jpg')";
+container.style.backgroundSize = "cover";
+container.style.backgroundRepeat = "no-repeat";
+container.style.height = "1000px";
+container.style.color = "white";
+container.style.textShadow = "1px 1px 5px black";
+container.style.textAlign = "center";
+content.appendChild(container);
